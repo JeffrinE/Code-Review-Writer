1a82435ac5394546586ed4dc91bc347095943b47
Jeffrin E <jeffrinedmond@gmail.com>
2024-09-01 19:01:38 +0530

    added editing page

--- pages/page_editing.py
+++ pages/page_editing.py
@@ -1,10 +1,33 @@
 import streamlit as st
+from app import llm_response_generator
 
 container = st.container(border= True)
+memory = []
+try:
+    generated_content = st.session_state.generated_content
+    switched = st.session_state.switch_page
+    memory.append(generated_content)
+except Exception as e:
+    generated_content = e
+    st.session_state.switch_page = False
 
-container.write(st.session_state.generated_content)
+if st.session_state.switch_page and not st.session_state.switched_page :
+    container.write(generated_content)
+    st.session_state.switched_page = True
+
 
 with st.sidebar:
     messages = st.container(height=300)
     if prompt := st.chat_input("Say something"):
         messages.chat_message("user").write(prompt)
+    try:
+        generated_content = llm_response_generator.editor_agent_response(st.session_state.generated_content, prompt, memory, st.session_state.llm)
+        st.session_state.generated_content = generated_content
+        container.write(st.session_state.generated_content)
+        memory.append(generated_content)
+    except Exception as e:
+        container.warning(f"Error occured: "
+                          f"{e}")
+
+if len(memory) > 3:
+    memory = memory[1:]
