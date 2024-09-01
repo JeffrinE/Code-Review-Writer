1292ae50ba61d6d1e3465405a1db153621a3dfcb
Jeffrin E <jeffrinedmond@gmail.com>
2024-09-01 19:01:23 +0530

    added option for editing

--- pages/page_main.py
+++ pages/page_main.py
@@ -4,7 +4,7 @@ import run
 import wx
 import streamlit as st
 from pages import page_editing
-from app import code_to_text, llm_response_generator
+from app import code_to_text, llm_response_generator, local_models, setModel
 
 st.title("Github ReadMe Writer")
 
@@ -18,7 +18,7 @@ def get_folder_path():
         return folder_path.GetPath()
     return None
 
-
+#sidebar
 sidebar = st.sidebar
 sidebar.header('This program assumes you have Ollama running')
 
@@ -27,13 +27,32 @@ if 'disabled_main_button' not in st.session_state:
 if 'main_folder_path' not in st.session_state:
     st.session_state.main_folder_path = None
 
+container_models = sidebar.container(border= True)
+if 'llm' not in st.session_state:
+    st.session_state.llm = ""
+if 'model' not in st.session_state:
+    st.session_state.model = False
+
+option = container_models.selectbox(
+    "Model",
+    local_models.get_local_models(),
+    index=None,
+    placeholder="Select model",
+    key= "model_selectable"
+)
+
+st.session_state.model = option
+
+if st.session_state.model:
+    llm = setModel.llm(st.session_state.model)
+    st.session_state.llm = llm.set_llm()
 
 def click_main():
     st.session_state.disabled_main_button = True
 
 
 container_browse_main = sidebar.container(border=True)
-container_browse_main.write("Upload Project Folder")
+container_browse_main.write("Project Folder")
 
 if container_browse_main.button("Upload", key="main_folder", on_click=click_main,
                              disabled=st.session_state.disabled_main_button):
@@ -54,7 +73,7 @@ def click_exclusion():
     st.session_state.exclusion_bt_clicked = True
 
 container_browse_excluded = sidebar.container(border= True)
-container_browse_excluded.write("Upload Excluded files")
+container_browse_excluded.write("Excluded Folders")
 
 if container_browse_excluded.button("Upload", key="excluded_folder", on_click= click_exclusion):
     path = get_folder_path()
@@ -77,11 +96,11 @@ options = st.multiselect(
 )
 
 sequence_list = [i for i in options]
-st.write(sequence_list)
+
 #generate
 
 if 'generated_content' not in st.session_state:
-    st.session_state.generated_content = ""
+    st.session_state.generated_content = False
 
 if 'generate_clicked' not in st.session_state:
     st.session_state.generate_clicked = False
@@ -93,22 +112,25 @@ st.button("Generate", key="generate", on_click= clicked_generate)
 
 container_response = st.container(border= True)
 
-# if st.session_state.generate_clicked:
-#     code_to_text_database = code_to_text.codeToText.codeToTextRun(st.session_state.main_folder_path, st.session_state.excluded_files)
-#     response = llm_response_generator.main(sequence_list)
-#     st.session_state.generated_content = response
-#     container_response.write(response)
-
-if st.session_state.generate_clicked:
-    st.write("Generated")
-    st.session_state.generated_content = "Test Response lorem epsum"
-
+if st.session_state.generate_clicked and not st.session_state.generated_content:
+    code_to_text_database = code_to_text.codeToText.codeToTextRun(st.session_state.main_folder_path, st.session_state.excluded_files)
+    llm_response_generator.set_llm(option)
+    # response = llm_response_generator.main(sequence_list)
+    # st.session_state.generated_content = response
+    # container_response.write(response)
+    with open(r"D:\PycharmProjects\Github-ReadMe-Writer\text.txt") as f:
+        file = f.read()
+    response = file
+    st.session_state.generated_content = response
+    container_response.write(response)
 
 # Switch Page
 if 'switch_page' not in st.session_state:
     st.session_state.switch_page = False
+
 def switch_page():
     st.session_state.switch_page = True
+    st.session_state.switched_page = False
 
 
 if st.session_state.generate_clicked:
