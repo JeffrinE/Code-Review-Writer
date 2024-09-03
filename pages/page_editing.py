9c50d34f44a92daff7b9de0ff0d1fbe0daff8107
Jeffrin E <jeffrinedmond@gmail.com>
2024-09-03 18:45:14 +0530

    updated editing page

--- pages/page_editing.py
+++ pages/page_editing.py
@@ -20,22 +20,22 @@ with st.sidebar:
     messages = st.container(height=100)
     if prompt := st.chat_input("User Prompt"):
         messages.chat_message("user").write(prompt)
-    try:
-        generated_content = llm_response_generator.editor_agent_response(st.session_state.generated_content, prompt,
-                                                                         memory, st.session_state.llm)
-        st.session_state.generated_content = generated_content
-        container.write(st.session_state.generated_content)
-        memory.append(generated_content)
-    except Exception as e:
-        container.warning(f"Error occured: "
-                          f"{e}")
+
+        try:
+            generated_content = llm_response_generator.editor_agent_response(st.session_state.generated_content, prompt,
+                                                                             memory, st.session_state.llm)
+            st.session_state.generated_content = generated_content
+            container.write(st.session_state.generated_content)
+            memory.append(generated_content)
+        except Exception as e:
+            container.warning(f"Sorry an error occured, refresh page")
 
     container_structure = st.container(border= True)
     container_structure.title(f"File Structure\n")
     try:
         container_structure.text(st.session_state.structure)
     except Exception as e:
-        container_structure.error(e)
+        pass
 
 if len(memory) > 3:
     memory = memory[1:]
